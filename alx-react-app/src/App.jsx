68867807d367540e571e07b42a2be3b47ec4332d
Lucas19940820 <l.chauke@lab.clicklearning.org>
2024-10-29 13:04:37 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
        <Header />
        <MainContent />
           <WelcomeMessage />
-          <UserProfile name='Alice' age='25' bio ='Loves hiking and photography'/>
+          <UserProfile name='Alice' age='25' bio='Loves hiking and photography'/>
            <Footer />
       </div>
       <div>
