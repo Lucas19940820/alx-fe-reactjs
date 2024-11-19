9432dad9fdbc0123efcf8cdd4da7546c1a4c20b8
Lucas19940820 <l.chauke@lab.clicklearning.org>
2024-11-19 09:45:35 +0200

    update file

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,31 +1,42 @@
 import React from "react";
-import {useQuery} from "react-query";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const fetchPosts = async () => {
-
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    if (!response.ok){
-        throw new Error("Network response was not ok");
-    }
-    return response.json();
+  const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return data;
 };
 
-const PostComponent = () => {
+const PostsComponent = () => {
+  const { data, isLoading, isError, error } = useQuery("posts", fetchPosts, {
+    // Advanced React Query options
+    cacheTime: 5 * 60 * 1000, // Cache data for 5 minutes
+    staleTime: 30 * 1000,     // Data is considered fresh for 30 seconds
+    refetchOnWindowFocus: false, // Do not refetch when window gains focus
+    keepPreviousData: true,   // Keep previous data while fetching new data
+  });
 
-    const {data, error, isLoading, isError} = useQuery("posts", fetchPosts);
-    if(isLoading) return <p>Loading posts....</p>;
-    if(isError) return <p>Error fetching posts: {error.message}</p>;
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-    return (
-        <div>
-            <h2>Posts</h2>
-            <button onClick={() => refetch()}>Refretch Posts</button>
-            <ul>
-                {data.map((post) => (
-                    <li key={post.id} >{post.title}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  if (isError) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
-export default PostComponent;
+
+export default PostsComponent;
