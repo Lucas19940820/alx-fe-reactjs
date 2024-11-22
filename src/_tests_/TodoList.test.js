57898be137e533ad7a210f6f866bfd63291f478f
Lucas <l.chauke@lab.clicklearning.org>
2024-11-22 09:12:27 +0200

    Upload react-todo project

--- src/_tests_/TodoList.test.js
+++ src/_tests_/TodoList.test.js
@@ -5,13 +5,13 @@ import TodoList from "../components/TodoList";
 
 describe("TodoList Component", () => {
   test("renders initial todos", () => {
-    render('<TodoList />');
+    render(<TodoList />);
     expect(screen.getByText("Learn React")).toBeInTheDocument();
     expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
   });
 
   test("adds a new todo", () => {
-    render('<TodoList />');
+    render(<TodoList />);
     const input = screen.getByRole("textbox");
     const addButton = screen.getByText("Add Todo");
 
@@ -21,14 +21,14 @@ describe("TodoList Component", () => {
   });
 
   test("toggles a todo", () => {
-    render('<TodoList />');
+    render(<TodoList />);
     const todoItem = screen.getByText("Learn React");
     fireEvent.click(todoItem);
     expect(todoItem).toHaveStyle("text-decoration: line-through");
   });
 
   test("deletes a todo", () => {
-    render('<TodoList />');
+    render(<TodoList />);
     const deleteButton = screen.getAllByText("Delete")[0];
     fireEvent.click(deleteButton);
     expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
