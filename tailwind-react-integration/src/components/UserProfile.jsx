b95a2f512260a897a90d98ead4667eff58ce7ff9
Lucas19940820 <l.chauke@lab.clicklearning.org>
2024-11-25 14:40:58 +0200

    update img tag

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,18 @@ function UserProfile() {
     return (
       <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
         <img
-          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+          className={[
+            "rounded-full",
+            "hover:scale-110",
+            "transition-transform",
+            "duration-300",
+            "ease-in-out",
+            "sm:w-24",
+            "sm:h-24",
+            "md:w-36",
+            "md:h-36",
+            "mx-auto",
+          ].join(" ")}
           src="https://via.placeholder.com/150"
           alt="User"
         />
